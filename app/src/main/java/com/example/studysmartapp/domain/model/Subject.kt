1c640e22a08a730208a3abf1f746744d45b54fd5
AlihanCelik <alihan_08032003@hotmail.com>
2024-08-28 14:48:06 +0300

    studySmart

---
+++ app/src/main/java/com/example/studysmartapp/domain/model/Subject.kt
@@ -0,0 +1,19 @@
+package com.example.studysmartapp.domain.model
+
+import androidx.compose.ui.graphics.Color
+import com.example.studysmartapp.ui.theme.gradient1
+import com.example.studysmartapp.ui.theme.gradient2
+import com.example.studysmartapp.ui.theme.gradient3
+import com.example.studysmartapp.ui.theme.gradient4
+import com.example.studysmartapp.ui.theme.gradient5
+
+data class Subject(
+    val name:String,
+    val goalHours:Float,
+    val colors:List<Color>
+){
+    companion object{
+        val subjectsCardColors= listOf(gradient1, gradient2, gradient3, gradient4, gradient4,
+            gradient5)
+    }
+}
