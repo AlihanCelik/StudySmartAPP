48ec378d2fb893d3a063a6038b10b1021b303736
AlihanCelik <alihan_08032003@hotmail.com>
2024-09-05 12:02:05 +0300

    studySmart

---
+++ app/src/main/java/com/example/studysmartapp/presentation/session/SessionScreen.kt
@@ -0,0 +1,215 @@
+package com.example.studysmartapp.presentation.session
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.studysmartapp.domain.model.Session
+import com.example.studysmartapp.presentation.components.DeleteDiaLog
+import com.example.studysmartapp.presentation.components.SubjectListBottomSheet
+import com.example.studysmartapp.presentation.components.studySessionsList
+import com.example.studysmartapp.sessions
+import com.example.studysmartapp.subjects
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SessionScreen(){
+    val sheetState= rememberModalBottomSheetState()
+    var isSubjectListBottomSheet by remember {
+        mutableStateOf(false)
+    }
+    var isDeleteDialogOpen by rememberSaveable {
+        mutableStateOf(false)
+    }
+    val scope= rememberCoroutineScope()
+
+    DeleteDiaLog(
+        isOpen = isDeleteDialogOpen,
+        title = "Delete Task?",
+        onDismissRequest = {isDeleteDialogOpen=false },
+        onConfirmButtonClick = {isDeleteDialogOpen=false},
+        onBodyText = "Are you sure,you want to delete this task ? " +
+                "This action can not be undone."
+    )
+
+    SubjectListBottomSheet(
+        isOpen = isSubjectListBottomSheet,
+        sheetState = sheetState,
+        subject = subjects,
+        onSubjectClicked ={
+            scope.launch { sheetState.hide() }.invokeOnCompletion {
+                if(!sheetState.isVisible) isSubjectListBottomSheet=false
+            }
+        },
+        onDismissReguest = {isSubjectListBottomSheet=false})
+
+    Scaffold (
+        topBar = {SessionScreenTopBar(onBackButtonClicked = {})}
+    ){paddingValues ->
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+            item {
+                TimerSection(modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(1f))
+            }
+            item {
+                RelatedToSubjectSection(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 12.dp),
+                    relatedToSubject = "English",
+                    selectSubjectButtonClick = {isSubjectListBottomSheet=true}
+                )
+            }
+            item{
+                ButtonsSection(
+                    modifier = Modifier.padding(12.dp).fillMaxWidth(),
+                    startButtonClick = {},
+                    cancelButtonClick = {},
+                    finishButtonClick = {})
+            }
+            studySessionsList(
+                sectionTitle = "RECENT STUDY SESSIONS",
+                emptyListText = "You don't have any upcoming tasks. \n" +
+                        "Click the + button in subject screen to add new task.",
+                sessions = sessions,
+                onDeleteClick = {isDeleteDialogOpen=true}
+            )
+
+        }
+
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun SessionScreenTopBar(
+    onBackButtonClicked:()->Unit
+){
+    TopAppBar(
+        navigationIcon = {
+            IconButton(onClick = onBackButtonClicked) {
+                Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "Navigation to Back Screen")
+
+            }
+        },
+        title = {
+            Text(text = "Study Sessions", style = MaterialTheme.typography.headlineSmall)
+        })
+
+}
+
+@Composable
+private fun TimerSection(
+    modifier: Modifier
+){
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.Center){
+        Box(modifier = Modifier
+            .size(250.dp)
+            .border(5.dp, MaterialTheme.colorScheme.surfaceVariant, CircleShape)
+        )
+        Text(text = "00:05:32",
+            style = MaterialTheme.typography.titleLarge.copy(fontSize = 45.sp))
+    }
+}
+
+@Composable
+private fun RelatedToSubjectSection(
+    modifier: Modifier,
+    relatedToSubject:String,
+    selectSubjectButtonClick:()->Unit
+){
+    Column(
+        modifier=modifier
+    ) {
+        Text(text = "Related to subject",
+            style = MaterialTheme.typography.bodySmall
+        )
+        Row (
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Text(text = "English",
+                style = MaterialTheme.typography.bodyLarge
+            )
+            IconButton(onClick = { selectSubjectButtonClick()}) {
+                Icon(
+                    imageVector = Icons.Default.ArrowDropDown,
+                    contentDescription = "Select Subject")
+
+            }
+
+        }
+
+    }
+
+}
+
+@Composable
+private fun ButtonsSection(
+    modifier: Modifier,
+    startButtonClick:()->Unit,
+    cancelButtonClick:()->Unit,
+    finishButtonClick:()->Unit
+){
+    Row (
+        modifier=modifier,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ){
+        Button(onClick = startButtonClick) {
+            Text(text = "Cancel",
+                modifier=Modifier
+                    .padding(horizontal = 10.dp, vertical = 5.dp))
+        }
+        Button(onClick = cancelButtonClick) {
+            Text(text = "Start",
+                modifier=Modifier
+                    .padding(horizontal = 10.dp, vertical = 5.dp))
+        }
+        Button(onClick = finishButtonClick) {
+            Text(text = "Finish",
+                modifier=Modifier
+                    .padding(horizontal = 10.dp, vertical = 5.dp))
+        }
+
+    }
+}
