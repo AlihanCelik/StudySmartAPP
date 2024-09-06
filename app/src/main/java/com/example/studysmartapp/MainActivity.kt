c87ebf3249f89d11f00a3eac254305433539d166
AlihanCelik <alihan_08032003@hotmail.com>
2024-09-06 15:05:37 +0300

    studySmart

--- app/src/main/java/com/example/studysmartapp/MainActivity.kt
+++ app/src/main/java/com/example/studysmartapp/MainActivity.kt
@@ -4,22 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavGraph
 import com.example.studysmartapp.domain.model.Session
 import com.example.studysmartapp.domain.model.Subject
 import com.example.studysmartapp.domain.model.Task
 import com.example.studysmartapp.presentation.NavGraphs
 import com.example.studysmartapp.ui.theme.StudySmartAPPTheme
 import com.ramcosta.composedestinations.DestinationsNavHost
-import com.ramcosta.composedestinations.spec.NavGraphSpec
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/example/studysmartapp/StudySmartApp.kt
@@ -0,0 +1,8 @@
+package com.example.studysmartapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class StudySmartApp:Application() {
+}
