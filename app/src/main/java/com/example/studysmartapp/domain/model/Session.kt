53692b31687cdc88405185b7c5d93c4d2b2ba9ee
AlihanCelik <alihan_08032003@hotmail.com>
2024-09-06 13:13:20 +0300

    studySmart

--- app/src/main/java/com/example/studysmartapp/domain/model/Session.kt
+++ app/src/main/java/com/example/studysmartapp/domain/model/Session.kt
@@ -1,11 +1,14 @@
 package com.example.studysmartapp.domain.model
 
-import kotlin.time.Duration
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
+@Entity
 data class Session(
     val sessionSubjctId: Int,
     val relatedToSubject: String,
     val date: Long,
     val duration: Long,
-    val sessionId:Int
+    @PrimaryKey(autoGenerate = true)
+    val sessionId:Int?=null
 )
--- app/src/main/java/com/example/studysmartapp/domain/model/Subject.kt
+++ app/src/main/java/com/example/studysmartapp/domain/model/Subject.kt
@@ -1,17 +1,21 @@
 package com.example.studysmartapp.domain.model
 
 import androidx.compose.ui.graphics.Color
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.example.studysmartapp.ui.theme.gradient1
 import com.example.studysmartapp.ui.theme.gradient2
 import com.example.studysmartapp.ui.theme.gradient3
 import com.example.studysmartapp.ui.theme.gradient4
 import com.example.studysmartapp.ui.theme.gradient5
 
+@Entity
 data class Subject(
     val name:String,
     val goalHours:Float,
     val colors:List<Color>,
-    val subjectId:Int
+    @PrimaryKey(autoGenerate = true)
+    val subjectId:Int?=null
 ){
     companion object{
         val subjectsCardColors= listOf(gradient1, gradient2, gradient3, gradient4,
